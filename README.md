1ba0881a787a01556bf0eab8bf415935a8bfeb2b
KotatuBot <kotatubot@gmail.com>
2019-10-06 23:18:54 +0900

    ss

---
+++ README.md
@@ -0,0 +1,26 @@
+# SS
+
+This command searches a specific string from a specific directory
+
+## How to use
+
+```
+ss -d "directory path" -s "Search keyword"
+
+[Example]
+
+
+ss -d "." -s "SELECT"
+```
+
+
+## Install
+
+python is required
+
+```
+.bashrc
+
+alias ss="python your_path/ss/ss_main.py"
+
+```
---
+++ ss_main.py
@@ -0,0 +1,36 @@
+import subprocess as sp
+import os
+import argparse
+
+def FileSearch(path,word,dir_path):
+
+    if path!=".":
+        full_path = dir_path + "/"+ path
+
+    else:
+        full_path = dir_path
+
+    cmd = "find {0} -type f -print | xargs grep '{1}'".format(full_path,word)
+    res = sp.Popen(cmd, shell=True, stdout=sp.PIPE)
+    stdout,strerr = res.communicate()
+    print(stdout)
+        
+        
+
+
+# args
+exe_cute_dir = os.environ['PWD']
+parser = argparse.ArgumentParser(description='Find word in file')
+
+parser.add_argument('-d', help='file path')
+parser.add_argument('-s', help='Search Keyword')
+
+args = parser.parse_args()
+
+
+if (args.d!=None and len(args.s)!=None):
+       FileSearch(args.d,args.s,exe_cute_dir) 
+else:
+    print("Please set an argument")
+    print("-d: Directory or file path")
+    print("-s : Find word")
